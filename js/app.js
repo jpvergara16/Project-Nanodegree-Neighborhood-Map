564be97dbba96a7915b530968adcd105da8fb6ed
Julius Vergara <juliuspvergara@gmail.com>
2017-05-29 13:21:23 -0700

    Added full 10 restaurant locations, and styled markers

--- js/app.js
+++ js/app.js
@@ -133,13 +133,52 @@ function initMap() {
   // LatLng locations for restaurants
   var locations = [
     {
-      title: 'Cham Soot Gol',
+      title: 'Cham Sut Gol',
       location:{lat: 33.773451, lng: -117.969679}
+    },
+    {
+      title: 'Gen Korean BBQ House',
+      location:{lat : 33.7410417,lng : -117.8197316}
+    },
+    {
+      title: 'Shik Do Rak',
+      location : {lat : 33.6880437, lng : -117.7715683},
+    },
+    {
+      title: 'Shik Do Rak',
+      location : {lat : 33.7744226, lng : -117.9634719},
+    },
+    {
+      title: 'Byul Daepo - Star BBQ',
+      location:{lat : 33.7741566, lng : -117.9872909},
+    },
+    {
+      title: 'Incheonwon BBQ House',
+      location:{lat : 33.769312, lng : -117.9555207},
+    },
+    {
+      title: 'Mr BBQ',
+      location:{lat : 33.8726711, lng : -117.8899635},
+    },
+    {
+      title: 'Cham Soot Gol',
+      location:{lat : 33.8386232, lng : -117.994411},
+    },
+    {
+      title: 'Mo Ran Gak Restaurant',
+      location:{lat : 33.7742704, lng : -117.9642242},
+    },
+    {
+      title: 'All That Barbeque',
+      location:{lat : 33.6956663, lng : -117.7988465},
     }
   ];
 
   var largeInfowindow = new google.maps.InfoWindow();
-  var bounds = new google.maps.LatLngBounds();
+
+  //Styling for markers, one for default, one for user mouse over.
+  var defaultIcon = makeMarkerIcon('2F5F8F');
+  var highlightedIcon = makeMarkerIcon('F0F5C3');
 
   // Loop through location array to create marker array on intialize
   for (var i = 0; i < locations.length; i++) {
@@ -152,6 +191,7 @@ function initMap() {
       position: position,
       title: title,
       animation: google.maps.Animation.DROP,
+      icon: defaultIcon,
       id: i
     });
     //Pushes the marker to marker array
@@ -160,10 +200,15 @@ function initMap() {
     marker.addListener('click', function() {
       populateInfoWindow(this, largeInfowindow);
     });
-    bounds.extend(markers[i].position);
+    // Two event listeners - one for mouseover, one for mouseout,
+    // to change the colors back and forth.
+    marker.addListener('mouseover', function() {
+      this.setIcon(highlightedIcon);
+    });
+    marker.addListener('mouseout', function() {
+      this.setIcon(defaultIcon);
+    });
   }
-  // Extend boundaries of map for each marker
-  map.fitBounds(bounds);
 }
 
 // Function populates infowindow when marker is clicked
@@ -172,10 +217,19 @@ function populateInfoWindow(marker, infowindow) {
   if (infowindow.marker != marker) {
     infowindow.marker = marker;
     infowindow.setContent('<div>' + marker.title + '</div>');
+    // Opens infowindowon correct marker
     infowindow.open(map, marker);
-    // Make sure the marker property is cleared if the infowindow is closed.
-    infowindow.addListener('closeclick',function(){
-      infowindow.setMarker = null;
-    });
   }
+}
+
+// This function takes in a COLOR, and then creates a new marker icon of that color.
+function makeMarkerIcon(markerColor) {
+  var markerImage = new google.maps.MarkerImage(
+    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+    '|40|_|%E2%80%A2',
+    new google.maps.Size(21, 34),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(10, 34),
+    new google.maps.Size(21,34));
+  return markerImage;
 }
