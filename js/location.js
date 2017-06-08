14c5497d97e673fb17c400a0b2d93953c2ea4a95
Julius Vergara <juliuspvergara@gmail.com>
2017-06-08 08:58:43 -0700

    Separated location data, google maps JS, and viewmodel js

---
+++ js/location.js
@@ -0,0 +1,73 @@
+/* ====== MODEL ======= */
+// LatLng locations for restaurants
+var locations = [{
+      title: 'Cham Sut Gol',
+      location: {
+          lat: 33.773451,
+          lng: -117.969679
+      }
+  },
+  {
+      title: 'Gen Korean BBQ House',
+      location: {
+          lat: 33.7410417,
+          lng: -117.8197316
+      }
+  },
+  {
+      title: 'Shik Do Rak',
+      location: {
+          lat: 33.6880437,
+          lng: -117.7715683
+      },
+  },
+  {
+      title: 'Shik Do Rak',
+      location: {
+          lat: 33.7744226,
+          lng: -117.9634719
+      },
+  },
+  {
+      title: 'Byul Daepo - Star BBQ',
+      location: {
+          lat: 33.7741566,
+          lng: -117.9872909
+      },
+  },
+  {
+      title: 'Incheonwon BBQ House',
+      location: {
+          lat: 33.769312,
+          lng: -117.9555207
+      },
+  },
+  {
+      title: 'Mr BBQ',
+      location: {
+          lat: 33.8726711,
+          lng: -117.8899635
+      },
+  },
+  {
+      title: 'Cham Soot Gol',
+      location: {
+          lat: 33.8386232,
+          lng: -117.994411
+      },
+  },
+  {
+      title: 'Mo Ran Gak Restaurant',
+      location: {
+          lat: 33.7742704,
+          lng: -117.9642242
+      },
+  },
+  {
+      title: 'All That Barbeque',
+      location: {
+          lat: 33.6956663,
+          lng: -117.7988465
+      },
+  }
+];
