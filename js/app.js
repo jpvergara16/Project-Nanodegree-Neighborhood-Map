a24c1a83cdd7eddc91ba39f76aec3c88e67709ad
Julius Vergara <juliuspvergara@gmail.com>
2017-07-18 02:39:07 -0700

    new: Refactored code into one js file, added more specific comments for separation, fixed foursquare & map bug of no display

--- js/app.js
+++ js/app.js
@@ -1,205 +1,318 @@
+//Initiates data binding & map when page loads
+$(document).ready(function() {
 /* ====== MODEL ======= */
 // Global variables
-var self = this;
-var map, infoWindow;
+  var self = this;
+  var map, infoWindow;
 
-// Creating our resLocations list - which holds all locations info( name, position, address, phone )
-var Model = [];
+// Array to hold all location info
+  var resLocations = [];
 
 /* ====== VIEWMODEL ======= */
-var ViewModel = function() {
-  var maxSectWidth = 767;
-  var self = this;
+  var ViewModel = function() {
 
-  var infoWindow = new google.maps.InfoWindow();
+    var self = this;
 
-  self.allVenues = ko.observableArray();
-  self.filteredVenues = ko.observableArray();
-  self.markers = ko.observableArray([]);
-  self.filter = ko.observable('');
-  self.allVenues.subscribe(function(newValue) {
-    filterFunction(self.filter(), self.allVenues, self.filteredVenues, self.markers);
-  });
-  self.filter.subscribe(function(newValue) {
-    filterFunction(newValue, self.allVenues, self.filteredVenues, self.markers);
-  });
+    var infoWindow = new google.maps.InfoWindow();
 
-	// to detect when window is resized
-	self.windowWidth = ko.observable(window.innerWidth);
-	// hides intro and filter section when browser is at certain size
-	self.hideSect = ko.observable(self.windowWidth() < maxSectWidth);
-
-  // Observables for food choices
-  self.food = ['Korean', 'Japanese', 'Chinese', 'Vietnamese', 'Filipino'];
-  self.foodChoice = ko.observable('Korean');
-  self.foodChoice.subscribe(function(newValue) {
-    updateMap(self.foodChoice, self.allVenues, true, self.markers, infoWindow);
-  });
-  updateMap(self.foodChoice, self.allVenues, false, self.markers, infoWindow);
-
-// Populate the marker of the Venue selected from the list
-  self.populateInfoFromItem = function(item) {
-    var marker = self.markers().find((mk) => {
-      return mk.title === item.name;
+    self.allVenues = ko.observableArray();
+    self.filteredVenues = ko.observableArray();
+    self.markers = ko.observableArray([]);
+    self.filter = ko.observable('');
+    self.allVenues.subscribe(function(newValue) {
+      filterFunction(self.filter(), self.allVenues, self.filteredVenues, self.markers);
     });
-    populateInfoWindow(marker, infoWindow);
+    self.filter.subscribe(function(newValue) {
+      filterFunction(newValue, self.allVenues, self.filteredVenues, self.markers);
+    });
+
+    // Observables for food selections
+    self.food = ['Korean', 'Japanese', 'Chinese', 'Filipino', 'Vietnamese', 'Mongolian'];
+    self.foodSelect = ko.observable('Korean');
+    self.foodSelect.subscribe(function(newValue) {
+      updateMap(self.foodSelect, self.allVenues, true, self.markers, infoWindow);
+    });
+    updateMap(self.foodSelect, self.allVenues, false, self.markers, infoWindow);
+
+  //Populates the marker of the Venue selected from the list
+    self.populateInfoFromItem = function(item) {
+      var marker = self.markers().find((mk) => {
+        return mk.title === item.name;
+      });
+      populateInfoWindow(marker, infoWindow);
+    };
+
   };
 
-	self.viewIsSmall = function() {
-		return self.windowWidth() < maxSectWidth;
-	};
-};
 
-// Filter the list of venues in the DOM into a new observableArray
-var filterFunction = function(term, allVenues, filteredVenues, markers) {
-  term = term.toUpperCase();
-  let tempArray = allVenues().filter((item) => {
-    return term ? (item.name.toUpperCase()).indexOf(term) > -1 : true;
-  });
-  filteredVenues.removeAll();
-  tempArray.forEach((item) => {
-    filteredVenues.push(item);
-  });
-};
+//functionality for filtering location in the DOM
+  var filterFunction = function(term, allVenues, filteredVenues, markers) {
+    term = term.toUpperCase();
+    let tempArray = allVenues().filter((item) => {
+      return term ? (item.name.toUpperCase()).indexOf(term) > -1 : true;
+    });
+    filteredVenues.removeAll();
+    tempArray.forEach((item) => {
+      filteredVenues.push(item);
+    });
+  };
 
-// Updating our map with markers and locations from the model
-// We are fetching data based on the user's food selection in the DOM
-var updateMap = function(foodChoice, allVenues, init, markers, infoWindow) {
 
-// Provide client id, client secret, and set up foursquare api
-// Foursqure Api necessities
-  var client_id = 'GHE2SLTC2WDUUN2V0NGQ2JJUZW12DNKABZWYWPM1AU5PNO2V';
-  var client_secret = 'WRW1ETHIVCIU12KR05BRIUVCDITOLGMD2PTYZMRZ42FFZ3ZS';
-  var base_url = 'https://api.foursquare.com/v2/';
-  var endpoint = 'venues/explore?&near=3900+Parkview+Ln+Irvine+CA&query=' + foodChoice() + '&limit=15';
-  var key = '&client_id=' + client_id + '&client_secret=' + client_secret + '&v=' + '20170704';
-  var url = base_url+endpoint+key;
+//Updating map with marker & location data. Locations based on the user food selection
+  var updateMap = function(foodSelect, allVenues, init, markers, infoWindow) {
+  /* === FOURSQUARE API CREDENTIALS === */
+    var client_id = 'GHE2SLTC2WDUUN2V0NGQ2JJUZW12DNKABZWYWPM1AU5PNO2V';
+    var client_secret = 'WRW1ETHIVCIU12KR05BRIUVCDITOLGMD2PTYZMRZ42FFZ3ZS';
+    var base_url = 'https://api.foursquare.com/v2/';
+    var endpoint = 'venues/explore?&near=Irvine+CA&query=' + foodSelect() + '&limit=15';
+    var key = '&client_id=' + client_id + '&client_secret=' + client_secret + '&v=' + '20170704';
+    var url = base_url+endpoint+key;
 
-// AJAX request from foursquare to populate our Model
-  $.getJSON(url, function( data ) {
+  // AJAX request from foursquare to populate our resLocations array
+    $.getJSON(url, function( data ) {
+      resLocations = [];
+      var foursquareLoc = data.response.groups[0].items;
 
-    Model = [];
-    var foursquareLoc = data.response.groups[0].items;
+    //Loop through the foursquare data and push each item into the Model
+      for (i = 0; i < foursquareLoc.length; i++) {
+      // Store our data variables for later use
+        var name = foursquareLoc[i].venue.name;
+        var lat = foursquareLoc[i].venue.location.lat;
+        var lng = foursquareLoc[i].venue.location.lng;
+        var position = {lng, lat};
+        var address = foursquareLoc[i].venue.location.formattedAddress;
+        var phone = foursquareLoc[i].venue.contact.formattedPhone;
+        var rating = foursquareLoc[i].venue.rating;
+      // Push our items returned to the locations array
+        resLocations.push({
+          name: name,
+          position: position,
+          address: address,
+          phone: phone,
+          rating: rating
+        });
+      }
 
-  // Loop through the foursquare data and push each item into the Model
-    for (i = 0; i < foursquareLoc.length; i++) {
-    // Store our data variables for later use
-      var name = foursquareLoc[i].venue.name;
-      var lat = foursquareLoc[i].venue.location.lat;
-      var lng = foursquareLoc[i].venue.location.lng;
-      var position = {lng, lat};
-      var address = foursquareLoc[i].venue.location.formattedAddress;
-      var phone = foursquareLoc[i].venue.contact.formattedPhone;
-      var rating = foursquareLoc[i].venue.rating;
-    // Push our items returned to the model
-      Model.push({
-        name: name,
-        position: position,
-        address: address,
-        phone: phone,
-        rating: rating
+      if (!init) {
+        var map = initMap(true, markers, infoWindow);
+      } else {
+        initMap(false, markers, infoWindow);
+      }
+
+    // Push each item in resLocations array to the allVenues array and remove after each food choice chosen
+      allVenues.removeAll();
+      resLocations = resLocations.sort(function(a, b) {
+        return a.name > b.name ? 1 : -1;
+      });
+      resLocations.forEach((item) => {
+        allVenues.push(item);
+      });
+
+  // Alert fail if data is unable to be retrieved from foursquare
+    }).fail(function () {
+      alert("Failed to retreive data!");
+    });
+  };
+
+
+/* ====== GOOGLEMAPS ======= */
+  var buildMap = function() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 11,
+        center: new google.maps.LatLng(33.7455, -117.8677),
+        styles: mapStyles,
+        mapTypeControl: false
+      });
+  };
+
+// intialize markers on load and extend bounds
+  function initMap(init, markers, infoWindow) {
+    if (init) {
+      buildMap();
+    }
+
+  // Set our markers to null each time choice is made to clear markers
+    for (var i = 0; i < markers().length; i++) {
+        markers()[i].setMap(null);
+    }
+    var bounds = new google.maps.LatLngBounds();
+
+    // Marker styles - this function takes in a COLOR, and then creates a new marker icon of that color.
+    function makeMarkerIcon(markerColor) {
+    var markerImage = new google.maps.MarkerImage(
+    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+    '|40|_|%E2%80%A2',
+    new google.maps.Size(21, 34),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(10, 34),
+    new google.maps.Size(21,34));
+    return markerImage;
+  }
+
+    //Styling for markers, one for default, one for user mouse over.
+    var defaultIcon = makeMarkerIcon('2F5F8F');
+    var highlightedIcon = makeMarkerIcon('F0F5C3');
+
+  // Loop through the locaitons array and set marker info for info window content
+    for (var i = 0; i < resLocations.length; i++) {
+      var locations = resLocations[i];
+      var latlng = locations.position;
+      var name = locations.name;
+      var address = locations.address;
+      var phone = locations.phone;
+      var rating = locations.rating;
+      var marker = new google.maps.Marker({
+        title: name,
+        position: latlng,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        icon: defaultIcon,
+        content: '<h2 style="margin-bottom: 0;">' + name + '</h2><h4>' + address[0] + '</br>' + address[1] + '</br>' + address[2] + '</h4><p style="font-weight: bold;">' + rating + '/10 Rating</p><a href="tel:"' + phone + '">' + phone + '</a>'
+      });
+      bounds.extend(resLocations[i].position);
+      markers.push(marker);
+      // Add event listeners for when markers are either clicked or focused on
+        marker.addListener('click', function() {
+          populateInfoWindow(this, infoWindow);
+        });
+        marker.addListener('mouseover', function() {
+          this.setIcon(highlightedIcon);
+        });
+        marker.addListener('mouseout', function() {
+          this.setIcon(defaultIcon);
+        });
+      map.fitBounds(bounds);
+    }
+  }
+
+  // simple function populates the infowindow when marker is clicked
+  function populateInfoWindow(marker, infowindow) {
+    infowindow.marker = null;
+    if (infowindow.marker != marker) {
+      infowindow.marker = marker;
+      infowindow.setContent(marker.content);
+      infowindow.open(map, marker);
+      infowindow.addListener('closeclick', function() {
+        infowindow.marker = null;
       });
     }
+  }
 
-    if (!init) {
-      var map = initMap(true, markers, infoWindow);
-    } else {
-      initMap(false, markers, infoWindow);
+  var mapStyles = [
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "saturation": 43
+            },
+            {
+                "lightness": -11
+            },
+            {
+                "hue": "#0088ff"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "hue": "#ff0000"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 99
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#808080"
+            },
+            {
+                "lightness": 54
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ece2d9"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ccdca1"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#767676"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.natural",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#EBE5E0"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.sports_complex",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
     }
+];
 
-  // Push each item in Model to the allVenues array and remove after each food choice chosen
-    allVenues.removeAll();
-    Model = Model.sort(function(a, b) {
-      return a.name > b.name ? 1 : -1;
-    });
-    Model.forEach((item) => {
-      allVenues.push(item);
-    });
-// Alert fail if data is unable to be retrieved from foursquare
-  }).fail(function () {
-    alert("Data failed to retrieve!");
-  });
-};
-
-var viewModel = new ViewModel();
-ko.applyBindings(viewModel);
-
-window.onresize = function() {
-    // Idea from http://stackoverflow.com/questions/10854179/how-to-make-window-size-observable-using-knockout
-    viewModel.windowWidth(window.innerWidth);
-};
-
-/*// Location function to call location properties
-var Location = function(data) {
-	var self = this;
-	self.title = data.title;
-	self.tags = data.type;
-	self.location = data.location;
-	self.mapMarker = null; // mapMarker will be updated when map markers are created
-	self.visible = ko.observable(true); // this property will allow to filter the list of locations in UI
-	return self;
-};
-
-var ViewModel = function() {
-    var maxSectWidth = 767;
-    var self = this;
-    self.locations = new ko.observableArray([]);
-
-    resLocations.forEach(function(location) {
-      self.locations.push(new Location(location));
-    });
-
-		self.tags = ko.observableArray(type); // type filtering is displayed in UI drop down
-    self.selectedFilter = ko.observable('undefined'); // updated when user selects a new tag from the drop down
-    self.selectedLocation = ko.observable(undefined); // updated when user clicks on a location
-
-    // to detect when window is resized
-    self.windowWidth = ko.observable(window.innerWidth);
-    // hides intro and filter section when browser is at certain size
-    self.hideSect = ko.observable(self.windowWidth() < maxSectWidth);
-
-    // BEHAVIOR
-    self.onLocClick = function(location, caller) {
-        if (location === self.selectedLocation()) {
-          self.selectedLocation('undefined');
-        } else {
-          self.selectedLocation(location);
-				}
-        // avoid circular reference
-        if (caller !== map) {
-        	map.markerClicked(location.mapMarker, location, viewModel);
-				}
-    };
-
-		self.onFilter = function(vm) {
-		var filterTag = vm.selectedFilter();
-		var markersToShow = [];
-		var markersToHide = [];
-		for (var i = 0; i < self.locations().length; i++) {
-				var currentLocation = self.locations()[i];
-				if (currentLocation.tags.includes(filterTag)) {
-						currentLocation.visible(true);
-						markersToShow.push(currentLocation.mapMarker);
-				} else {
-						currentLocation.visible(false);
-						markersToHide.push(currentLocation.mapMarker);
-				}
-			}
-			map.showMarkers(markersToShow);
-			map.hideMarkers(markersToHide);
-    };
-
-		//filters selection when called
-		self.hideFilterSection = function() {
-		 self.hideSect(true);
-	 	};
-
-	 	self.showFilterSection = function() {
-		 self.hideSect(false);
-	 	};
-
-    self.viewIsSmall = function() {
-      return self.windowWidth() < maxSectWidth;
-    };
-
-};
+  ko.applyBindings(new ViewModel());
+});
